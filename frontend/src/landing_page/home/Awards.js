7f5d7b551547f7e578aae3412f40c2669df39a07
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-07-04 16:33:03 +0530

    hero component

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,10 +1,52 @@
-import React from 'react';
-
+import React from "react";
 
 function Awards() {
-    return ( 
-        <h1>Awards</h1>
-     );
+  return (
+    <div className="contianer mt-5">
+      <div className="row">
+        <div className="col-6 p-5">
+          <img src="media/images/largestBroker.svg" />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>Largest Stock broker in India</h1>
+          <p className="mb-5">
+            2+ million Zerodha client contribute to over 15% of all volumes in
+            India daily by Trading and Investing
+          </p>
+          <div className="row">
+            <div className="col-6">
+                <ul>
+            <li>
+              <p>Features and options</p>
+            </li>
+            <li>
+              <p>Commdodity derivatives</p>
+            </li>
+            <li>
+                <p>Currency derivatives</p>
+            </li>
+          </ul>
+            </div>
+            <div className="col-6">
+                <ul>
+            <li>
+              <p>Stocks and IPOs</p>
+            </li>
+            <li>
+              <p>Direct Matual funds</p>
+            </li>
+            <li>
+                <p>Bonds and Govt. Securities</p>
+            </li>
+          </ul>
+            </div>
+          </div>
+          <img src = '/media/images\pressLogos.png' style ={{width: "90%"}} />
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -3,7 +3,15 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <img src ='media/images/homeHero.png' alt='Hero Image' className='mb-5'/>
+                <h1 className='mt-5'>    Invest in Everything</h1>
+                <p>Online platform to invest in stock, derivatives,matual fund and more</p>
+                <button class = 'p-2 btn btn-primary fs-5 mb-5' style = {{width: "15%", margin: "0 auto"}}>signup now</button>
+            </div>
+
+        </div>
     );
 }
 
