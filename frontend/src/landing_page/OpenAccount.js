9ec36ca2db737479c496cbd5d25a4b8be3cef519
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-07-06 15:55:37 +0530

    navigate routers endpoint

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -3,7 +3,14 @@ import React from 'react';
 
 function OpenAccount() {
     return ( 
-        <h1>Open Account</h1>
+      <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <h1 className='mt-0'>    Open a Zerodha Account</h1>
+                <p className='mt-3'>Modern platform and apps, ₹0 investment, and flat ₹20 intrady and F&O trades</p>
+                <button class = 'p-2 btn btn-primary fs-5 mb-5 mt-3' style = {{width: "15%", margin: "0 auto"}}>Sign up now</button>
+            </div>
+
+        </div>
      );
 }
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
                 <img src ='media/images/homeHero.png' alt='Hero Image' className='mb-5'/>
                 <h1 className='mt-0'>    Invest in Everything</h1>
                 <p>Online platform to invest in stock, derivatives,matual fund and more</p>
-                <button class = 'p-2 btn btn-primary fs-5 mb-5' style = {{width: "15%", margin: "0 auto"}}>signup now</button>
+                <button class = 'p-2 btn btn-primary fs-5 mb-5' style = {{width: "15%", margin: "0 auto"}}>Signup now</button>
             </div>
 
         </div>
