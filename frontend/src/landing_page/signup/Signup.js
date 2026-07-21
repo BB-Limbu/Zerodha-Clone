e42c8b8db234b97c8244c899527b4621b98acfd9
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-07-21 16:45:18 +0530

    modified

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -3,7 +3,7 @@ import React from "react";
 function Signup() {
   return (
     <div className="container" id="signHero">
-      <div className="row text-center p-5">
+      <div className="row text-center p-5 mb-5">
         <h2 className="mt-5">Open a free demat and trading account online</h2>
         <p className="mt-3 text-muted fs-4">
           Start investing brokerage free and join a community of 1.6+ crore
@@ -15,7 +15,7 @@ function Signup() {
           <img src="./media/images/account_open.svg" />
         </div>
         <div className="col">
-          <h2>Signup now</h2>
+          <h2 className="fs-4">Signup now</h2>
           <p className="text-muted mt-3 ">Or track your existing application</p>
           <div className="d-flex mt-4">
             <span className="input-group-text rounded-start-0 rounded-end-0 border w-5 gap-2">
@@ -54,17 +54,128 @@ function Signup() {
           </p>
         </div>
       </div>
-      
-      <div className="text-center mt-5">
+
+      <div className="text-center mt-5 mb-5">
         <h2>Already have a demat account?</h2>
-        <p className="text-muted mt-4">Move your holdings to Zerodha and we'll cover your transfer costs, up to ₹500, learn more.</p>
-        <h2  className="fs-2" style={{marginTop:"10%"}}>Investment options with Zerodha demat account</h2>
+        <p className="text-muted mt-4">
+          Move your holdings to Zerodha and we'll cover your transfer costs, up
+          to ₹500,{" "}
+          <a href="#" style={{ textDecoration: "none" }}>
+            learn more
+          </a>
+          .
+        </p>
+        <h2 className="fs-2" style={{ marginTop: "10%" }}>
+          Investment options with Zerodha demat account
+        </h2>
       </div>
 
-      <div className="row">
-        <div className="col-4"></div>
-        <div className="col-4"></div>
-        <div className="col-4"></div>
+      <div className="mt-5">
+        <div className="row">
+          <div className="col-3">
+            <img
+              src="./media/images/stocks-acop.svg"
+              style={{ marginLeft: "50%" }}
+            />
+
+            <br />
+            <img
+              src="./media/images/ipo-acop.svg"
+              style={{ marginLeft: "50%", marginTop: "30%" }}
+            />
+          </div>
+          <div className="col-3">
+            <h2>Stocks</h2>
+            <p className="text-muted">
+              Invest in all exchange-listed securities
+            </p>
+            <h2 style={{ marginTop: "30%" }}>IPO</h2>
+            <p className="text-muted">
+              Apply to the latest IPOs instantly via UPI
+            </p>
+          </div>
+          <div className="col-3">
+            <img
+              src="./media/images/mf-acop.svg"
+              style={{ marginLeft: "50%" }}
+            />
+            <br />
+            <img
+              src="./media/images/fo-acop.svg"
+              style={{ marginLeft: "50%", marginTop: "40%" }}
+            />
+          </div>
+          <div className="col-3">
+            <h2>Mutual funds</h2>
+            <p className="text-muted">
+              Invest in commission-free direct mutual funds
+            </p>
+
+            <h2 style={{ marginTop: "30%" }}>Futures & options</h2>
+            <p className="text-muted">
+              Hedge and mitigate market risk through simplified F&O trading
+            </p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <button className=" mb-5 mt-5 input-group-text rounded-start rounded-end btn btn-primary w-40 py-2 fs-5">
+            Explore Investment
+          </button>
+        </div>
+      </div>
+
+      <div
+        className=" text-muted"
+        style={{ marginTop: "10%", marginBottom: "10%" }}
+      >
+        <div className="row">
+          <h2 className="text-center fs-4">
+            Steps to open a demat account with Zerodha
+          </h2>
+          <div className="col-6 mt-5">
+            <img
+              src="./media/images/steps-acop.svg"
+              style={{ width: "80%", marginLeft: "30px" }}
+            />
+          </div>
+          <div className="col-6 mt-5 mb-5" style={{ lineHeight: "3em" }}>
+            <div className="account-open-step">
+              <div
+                className="acop-step d-inline-flex justify-content-center align-items-center border rounded-circle"
+                style={{ width: "30px", height: "30px" }}
+              >
+                01
+              </div>
+              <p className="d-inline-block fw-medium ms-3 fs-5">
+                Enter the requested details
+              </p>
+            </div>
+
+            <div className="account-open-step">
+              <div
+                className="acop-step d-inline-flex justify-content-center align-items-center border rounded-circle"
+                style={{ width: "30px", height: "30px" }}
+              >
+                02
+              </div>
+              <p className="d-inline-block fw-medium ms-3 fs-5">
+                Complete e-sign & verification
+              </p>
+            </div>
+
+            <div className="account-open-step">
+              <div
+                className="acop-step d-inline-flex justify-content-center align-items-center border rounded-circle"
+                style={{ width: "30px", height: "30px" }}
+              >
+                03
+              </div>
+              <p className="d-inline-block fw-medium ms-3 fs-5">
+                Start investing!
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
